9b4e413bf5c9965bdf8d10fa7acef72c6da0e8b6
trabelsi houssem <trabelsi.houssem@hotmail.com>
2023-06-19 17:02:48 +0200

    Ajouter une fonction "modifier données de client"

--- affichage.js
+++ affichage.js
@@ -13,27 +13,32 @@ window.addEventListener("DOMContentLoaded", function () {
     // Crée un élément div et ajoute les données client à l'intérieur
   }
 
-  // Effacer les données du localStorage après les avoir affichées
-  localStorage.removeItem("clientData");
-
   // Supprimer les données lorsque le bouton est cliqué
   var supprimerButton = document.getElementById("supprimer");
   supprimerButton.addEventListener("click", function () {
     document.getElementById("affichage").innerHTML = "";
     // Efface le contenu de l'élément avec l'ID 'affichage'
+    localStorage.removeItem("clientData");
+    // Supprime les données du localStorage
   });
 
   // Modifier les données lorsque le bouton de modification est cliqué
   var modifierButton = document.getElementById("modifier");
   modifierButton.addEventListener("click", function () {
-    var newClientData = prompt(
-      "Veuillez entrer les nouvelles données du client :"
-    );
-    if (newClientData) {
-      document.getElementById("affichage").innerHTML = newClientData;
-      localStorage.setItem("clientData", newClientData);
-      // Demande à l'utilisateur de saisir de nouvelles données client,
-      // les affiche et les enregistre dans le localStorage
+    var entreprise = prompt("Veuillez entrer le nouveau nom de l'entreprise :");
+    var date = prompt("Veuillez entrer la nouvelle date :");
+    var description = prompt("Veuillez entrer la nouvelle description :");
+
+    var clientData = 
+    
+                      '<h2>' + entreprise + '</h2>' +
+                     '<p>Date de passage : ' + date + '</p>' +
+                     '<p>Description : ' + description + '</p>';
+
+    if (entreprise || date || description) {
+      document.getElementById("affichage").innerHTML = clientData;
+      localStorage.setItem("clientData", clientData);
+      // Met à jour les données dans l'affichage et dans le localStorage
     }
   });
 });
--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 document.getElementById('ajoutForm').addEventListener('submit', function(e) {
-    // Écouteur d'événement sur la soumission du formulaire avec l'identifiant 'ajoutForm'
-    e.preventDefault(); // Empêche le comportement par défaut du formulaire
-  
-    // Récupération des valeurs des champs du formulaire
-    var entreprise = document.getElementById('entreprise').value;
-    var date = document.getElementById('date').value;
-    var description = document.getElementById('description').value;
-  
-    // Construction des données du client
-    var clientData = '<h2>' + entreprise + '</h2>' +
-                     '<p>Date de passage : ' + date + '</p>' +
-                     '<p>Description : ' + description + '</p>';
-  
-    // Envoi des données à la page d'affichage via localStorage
-    localStorage.setItem('clientData', clientData);
-  
-    // Redirection vers la page d'affichage
-    window.location.href = 'affichage.html';
-  });
-  
+  // Écouteur d'événement sur la soumission du formulaire avec l'identifiant 'ajoutForm'
+  e.preventDefault(); // Empêche le comportement par défaut du formulaire
+
+  // Récupération des valeurs des champs du formulaire
+  var entreprise = document.getElementById('entreprise').value;
+  var date = document.getElementById('date').value;
+  var description = document.getElementById('description').value;
+
+  // Construction des données du client
+  var clientData = '<h2>' + entreprise + '</h2>' +
+                   '<p>Date de passage : ' + date + '</p>' +
+                   '<p>Description : ' + description + '</p>';
+
+  // Envoi des données à la page d'affichage via localStorage
+  localStorage.setItem('clientData', clientData);
+
+  // Redirection vers la page d'affichage
+  window.location.href = 'affichage.html';
+});
